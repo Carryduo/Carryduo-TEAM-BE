034bf3bf28f0e9e3c84446d1734e12f88574bb0e
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-08 23:38:28 +0900

    build: CD script 추가

---
+++ scrpits/after.install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo chmod -R 777 /home/ubuntu/build
+
+#navigate into our working directory
+cd /home/ubuntu/build
+
+#install node modules & update swagger & pm2 reload
+sudo npm install
+sudo pm2 reload carryduo
