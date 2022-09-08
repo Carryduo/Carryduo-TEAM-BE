888aa3e691825c4216c88eb8b684b36c55da00aa
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-08 22:10:30 +0900

    build: ecosystem 수정

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000
-  await app.listen(port, () => {
-    process.send('ready')
-  console.log(`application is listening on port ${port}...`)
-  });
-  process.on('SIGINT', function () {
-    app.close(function () {
-    console.log('server closed')
-    process.exit(0)
-    })
+  const port = 3000;
+  await app.listen(port);
 }
 
 bootstrap();
