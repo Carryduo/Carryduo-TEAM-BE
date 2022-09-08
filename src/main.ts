30cdad30fb2be1e06181aa180d3b1bd648f74276
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-08 21:59:02 +0900

    build: ecosystem 클러스터 모드로 변경

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000
+  await app.listen(port, () => {
+    process.send('ready')
+  console.log(`application is listening on port ${port}...`)
+  });
+  process.on('SIGINT', function () {
+    app.close(function () {
+    console.log('server closed')
+    process.exit(0)
+    })
 }
+
 bootstrap();
