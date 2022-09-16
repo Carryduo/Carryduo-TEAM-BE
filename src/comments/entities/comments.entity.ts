90bf71385208d90b298e6671d450acf33367d1fa
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-16 15:01:06 +0900

    docs: 구독/평판/조합승률 스웨거 적용

--- src/comments/entities/comments.entity.ts
+++ src/comments/entities/comments.entity.ts
@@ -4,21 +4,37 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { ChampEntity } from 'src/champ/entities/champ.entity';
 import { SummonerEntity } from 'src/summoner/entities/summoner.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'COMMENT',
 })
 export class CommentEntity extends CommonEntity {
+  @ApiProperty({
+    example: 'summoner/champ',
+    description: '평판글의 소속 카테고리(소환사에 대한/챔피언에 대한)',
+    required: false,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   category: string;
 
+  @ApiProperty({
+    example: '짱입니다',
+    description: '평판글 내용',
+    required: false,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty({
+    example: '2',
+    description: '신고 받은 횟수',
+    required: false,
+  })
   @Column({ type: 'int', nullable: false })
   @IsNumber()
   @IsNotEmpty()
@@ -27,6 +43,11 @@ export class CommentEntity extends CommonEntity {
   @ManyToOne(() => UserEntity, (user: UserEntity) => user.id, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
+  })
+  @ApiProperty({
+    example: 'xzczcaQWWWE23',
+    description: '글 작성자 ID',
+    required: false,
   })
   @JoinColumn([
     {
@@ -40,13 +61,18 @@ export class CommentEntity extends CommonEntity {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
+  @ApiProperty({
+    example: '56',
+    description: '챔피언 ID',
+    required: false,
+  })
   @JoinColumn([
     {
       name: 'champId',
       referencedColumnName: 'id',
     },
   ])
-  champId: UserEntity;
+  champId: ChampEntity;
 
   @ManyToOne(
     () => SummonerEntity,
@@ -56,11 +82,16 @@ export class CommentEntity extends CommonEntity {
       onUpdate: 'CASCADE',
     },
   )
+  @ApiProperty({
+    example: 'xzczcaQWWWE23',
+    description: '소환사 ID',
+    required: false,
+  })
   @JoinColumn([
     {
       name: 'summonerId',
       referencedColumnName: 'id',
     },
   ])
-  summonerId: UserEntity;
+  summonerId: SummonerEntity;
 }
