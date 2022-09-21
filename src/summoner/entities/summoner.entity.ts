3f46ec3a0f483895512e77d2742f15977aac5aa3
rlasuddn <ywaves073@gmail.com>
2022-09-21 20:17:41 +0900

    [#8] feat 유저 전적 검색API 2차 완성
    summonerDTO ApiProperty 추가,
    summonerEntity ApiProperty 추가,
    summonerAPI swagger 적용,
    유저 전적 업데이트 기능 추가,
    라이엇API에서 주는 status 코드로 response 추가

--- src/summoner/entities/summoner.entity.ts
+++ src/summoner/entities/summoner.entity.ts
@@ -4,46 +4,87 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ChampEntity } from 'src/champ/entities/champ.entity';
 import { SubscriptionEntity } from 'src/subscription/entities/subscription.entity';
 import { CommentEntity } from 'src/comments/entities/comments.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'SUMMONER',
 })
 export class SummonerEntity extends CommonEntity {
+  @ApiProperty({
+    example: '할배탈',
+    description: '소환사 이름',
+    required: true,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   summonerName: string;
 
+  @ApiProperty({
+    example: 'example.png',
+    description: '소환사 아이콘',
+    required: true,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   summonerIcon: string;
 
+  @ApiProperty({
+    example: 'SILVER I',
+    description: '소환사 티어',
+    required: true,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   tier: string;
 
+  @ApiProperty({
+    example: 'example.png',
+    description: '소환사 티어 이미지',
+    required: true,
+  })
   @Column({ type: 'varchar', nullable: false })
   @IsString()
   @IsNotEmpty()
   tierImg: string;
 
+  @ApiProperty({
+    example: '98',
+    description: '소환사 리그 포인트',
+    required: true,
+  })
   @Column({ type: 'int', nullable: false })
   @IsNumber()
   @IsNotEmpty()
   lp: number;
 
+  @ApiProperty({
+    example: '75',
+    description: '소환사 승리 수',
+    required: true,
+  })
   @Column({ type: 'int', nullable: false })
   @IsNumber()
   @IsNotEmpty()
   win: number;
 
+  @ApiProperty({
+    example: '55',
+    description: '소환사 패배 수',
+    required: true,
+  })
   @Column({ type: 'int', nullable: false })
   @IsNumber()
   @IsNotEmpty()
   lose: number;
 
+  @ApiProperty({
+    example: '57',
+    description: '소환사 승률',
+    required: true,
+  })
   @Column({ type: 'int', nullable: false })
   @IsNumber()
   @IsNotEmpty()
@@ -53,6 +94,11 @@ export class SummonerEntity extends CommonEntity {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
     eager: true,
+  })
+  @ApiProperty({
+    example: '81',
+    description: '모스트 챔피언1',
+    required: true,
   })
   @JoinColumn([
     {
@@ -67,6 +113,11 @@ export class SummonerEntity extends CommonEntity {
     onUpdate: 'CASCADE',
     eager: true,
   })
+  @ApiProperty({
+    example: '412',
+    description: '모스트 챔피언2',
+    required: true,
+  })
   @JoinColumn([
     {
       name: 'mostChamp2',
@@ -79,6 +130,11 @@ export class SummonerEntity extends CommonEntity {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
     eager: true,
+  })
+  @ApiProperty({
+    example: '222',
+    description: '모스트 챔피언3',
+    required: true,
   })
   @JoinColumn([
     {
--- src/summoner/summoner.service.ts
+++ src/summoner/summoner.service.ts
@@ -1,6 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { isArray } from 'class-validator';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SummonerInfoDTO } from './dto/summoner.dto';
 import { SummonerRepository } from './summoner.repository';
 
@@ -11,134 +10,157 @@ export class SummonerService {
     private readonly axios: HttpService,
   ) {}
 
-  async refreshSummonerInfo(summonerName: string) {
-    const response = await this.axios
-      .get(
-        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/ + ${encodeURIComponent(
-          summonerName,
-        )} + ?api_key=${process.env.RIOT_API_KEY}`,
-      )
-      .toPromise();
-
-    const { data } = response;
-
-    const summonerIcon = `https://ddragon.leagueoflegends.com/cdn/12.17.1/img/profileicon/${data.profileIconId}.png`;
-
-    const detailResponse = await this.axios
-      .get(
-        `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${data.id}?api_key=${process.env.RIOT_API_KEY}`,
-      )
-      .toPromise();
-
-    const mostChampResponse = await this.axios
-      .get(
-        `https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${data.id}/top?count=3&api_key=${process.env.RIOT_API_KEY}
-        `,
-      )
-      .toPromise();
-
-    const mostchamp = mostChampResponse.data;
-    const mostchampId = [
-      mostchamp[0].championId,
-      mostchamp[1].championId,
-      mostchamp[2].championId,
-    ];
-
-    const detailData = detailResponse.data[0];
-    const win = detailData.wins;
-    const lose = detailData.losses;
-
-    const winRate = Math.floor((win / (win + lose)) * 100);
-    let summonerInfo;
-    //디비에 유저 전적 데이터가 없을 경우 summonerInfo를 return 해주고 넣어준다.
-    let image;
-    switch (detailData.tier) {
-      case 'IRON':
-        image = 'https://erunjrun.com/tier/Iron.png';
-        break;
-      case 'BRONZE':
-        image = 'https://erunjrun.com/tier/Bronze.png';
-        break;
-      case 'SILVER':
-        image = 'https://erunjrun.com/tier/Silver.png';
-        break;
-      case 'GOLD':
-        image = 'https://erunjrun.com//tier/Gold.png';
-        break;
-      case 'PLATINUM':
-        image = 'https://erunjrun.com/tier/Platinum.png';
-        break;
-      case 'DIAMOND':
-        image = 'https://erunjrun.com/tier/Diamond.png';
-        break;
-      case 'MASTER':
-        image = 'https://erunjrun.com/tier/Master.png';
-        break;
-      case 'GRANDMASTER':
-        image = 'https://erunjrun.com/tier/Grandmaster.png';
-        break;
-      case 'CHALLENGER':
-        image = 'https://erunjrun.com/tier/Challenger.png';
-        break;
-    }
-    summonerInfo = {
-      summonerName: data.name,
-      summonerIcon,
-      tier: detailData.tier + ' ' + detailData.rank,
-      lp: detailData.leaguePoints,
-      tierImg: image,
-      win: detailData.wins,
-      lose: detailData.losses,
-      winRate,
-      mostChamp1: mostchamp[0].championId,
-      mostChamp2: mostchamp[1].championId,
-      mostChamp3: mostchamp[2].championId,
-    };
-
-    let findSummoner = await this.summonerRepository.findSummoner(data.name);
-    let summonerData;
-
-    if (!findSummoner) {
-      await this.summonerRepository.insertSummoner(summonerInfo);
-      findSummoner = await this.summonerRepository.findSummoner(data.name);
-      summonerData = {
-        summonerName: findSummoner.summonerName,
-        summonerIcon: findSummoner.summonerIcon,
-        tier: findSummoner.tier,
-        lp: findSummoner.lp,
-        tierImg: findSummoner.tierImg,
-        win: findSummoner.win,
-        lose: findSummoner.lose,
-        winRate: findSummoner.winRate,
-        mostChamps: [
-          findSummoner.mostChamp1,
-          findSummoner.mostChamp2,
-          findSummoner.mostChamp3,
-        ],
-      };
-      return summonerData;
-    }
-    await this.summonerRepository.updateSummoner(summonerInfo);
-    summonerData = {
-      summonerName: findSummoner.summonerName,
-      summonerIcon: findSummoner.summonerIcon,
-      tier: findSummoner.tier,
-      lp: findSummoner.lp,
-      tierImg: findSummoner.tierImg,
-      win: findSummoner.win,
-      lose: findSummoner.lose,
-      winRate: findSummoner.winRate,
+  async summonerDataCleansing(summoner: SummonerInfoDTO) {
+    const summonerData = {
+      summonerName: summoner.summonerName,
+      summonerIcon: summoner.summonerIcon,
+      tier: summoner.tier,
+      lp: summoner.lp,
+      tierImg: summoner.tierImg,
+      win: summoner.win,
+      lose: summoner.lose,
+      winRate: summoner.winRate,
       mostChamps: [
-        findSummoner.mostChamp1,
-        findSummoner.mostChamp2,
-        findSummoner.mostChamp3,
+        summoner.mostChamp1,
+        summoner.mostChamp2,
+        summoner.mostChamp3,
       ],
     };
     return summonerData;
-    await this.summonerInfo(data.name);
   }
 
-  async summonerInfo(summonerName: string) {
-    await this.summonerRepository.summonerInfo(summonerName);
+  async findSummoner(summonerName: string) {
+    const summoner = await this.summonerRepository.findSummoner(summonerName);
+    if (!summoner) {
+      const result = await this.summonerRiotRequest(summonerName);
+      await this.summonerRepository.insertSummoner(result);
+      const summonerInfo = await this.summonerRepository.findSummoner(
+        summonerName,
+      );
+      return await this.summonerDataCleansing(summonerInfo);
+    }
+    return await this.summonerDataCleansing(summoner);
+  }
+
+  async updateSummoner(summonerName: string) {
+    const summoner = await this.summonerRepository.findSummoner(summonerName);
+    if (!summoner)
+      throw new HttpException(
+        '갱신할 수 없는 소환사입니다.(DB에 소환사가 없습니다.)',
+        HttpStatus.BAD_REQUEST,
+      );
+    const result = await this.summonerRiotRequest(summonerName);
+    await this.summonerRepository.updateSummoner(result);
+    const summonerInfo = await this.summonerRepository.findSummoner(
+      summonerName,
+    );
+    const summonerData = await this.summonerDataCleansing(summonerInfo);
+    return summonerData;
+  }
+
+  async getSummoner(summonerName: string) {
+    const summoner = await this.findSummoner(summonerName);
+    return summoner;
+  }
+
+  async summonerRiotRequest(summonerName: string) {
+    try {
+      const response = await this.axios
+        .get(
+          `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/ + ${encodeURIComponent(
+            summonerName,
+          )} + ?api_key=${process.env.RIOT_API_KEY}`,
+        )
+        .toPromise();
+
+      const { data } = response;
+
+      const summonerIcon = `https://ddragon.leagueoflegends.com/cdn/12.17.1/img/profileicon/${data.profileIconId}.png`;
+
+      const detailResponse = await this.axios
+        .get(
+          `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${data.id}?api_key=${process.env.RIOT_API_KEY}`,
+        )
+        .toPromise();
+
+      const mostChampResponse = await this.axios
+        .get(
+          `https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${data.id}/top?count=3&api_key=${process.env.RIOT_API_KEY}
+        `,
+        )
+        .toPromise();
+      const mostchamp = mostChampResponse.data;
+
+      const detailData = detailResponse.data[0];
+
+      if (!detailData)
+        throw new HttpException(
+          '언랭크 소환사 입니다.',
+          HttpStatus.BAD_REQUEST,
+        );
+
+      const win = detailData.wins;
+      const lose = detailData.losses;
+
+      const winRate = Math.floor((win / (win + lose)) * 100);
+      let image: string;
+      switch (detailData.tier) {
+        case 'IRON':
+          image = 'https://erunjrun.com/tier/Iron.png';
+          break;
+        case 'BRONZE':
+          image = 'https://erunjrun.com/tier/Bronze.png';
+          break;
+        case 'SILVER':
+          image = 'https://erunjrun.com/tier/Silver.png';
+          break;
+        case 'GOLD':
+          image = 'https://erunjrun.com//tier/Gold.png';
+          break;
+        case 'PLATINUM':
+          image = 'https://erunjrun.com/tier/Platinum.png';
+          break;
+        case 'DIAMOND':
+          image = 'https://erunjrun.com/tier/Diamond.png';
+          break;
+        case 'MASTER':
+          image = 'https://erunjrun.com/tier/Master.png';
+          break;
+        case 'GRANDMASTER':
+          image = 'https://erunjrun.com/tier/Grandmaster.png';
+          break;
+        case 'CHALLENGER':
+          image = 'https://erunjrun.com/tier/Challenger.png';
+          break;
+      }
+      const summonerData = {
+        summonerName: data.name,
+        summonerIcon,
+        tier: detailData.tier + ' ' + detailData.rank,
+        lp: detailData.leaguePoints,
+        tierImg: image,
+        win: detailData.wins,
+        lose: detailData.losses,
+        winRate,
+        mostChamp1: mostchamp[0].championId,
+        mostChamp2: mostchamp[1].championId,
+        mostChamp3: mostchamp[2].championId,
+      };
+      return summonerData;
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 429) {
+        throw new HttpException(
+          '라이엇API 요청 과도화',
+          HttpStatus.TOO_MANY_REQUESTS,
+        );
+      } else if (err.response.status === 403) {
+        throw new HttpException('라이엇API 키 만료', HttpStatus.FORBIDDEN);
+      } else if (err.status === 400) {
+        throw new HttpException(err.response, err.status);
+      } else {
+        throw new HttpException(err.response.statusText, err.response.status);
+      }
+    }
   }
 }
