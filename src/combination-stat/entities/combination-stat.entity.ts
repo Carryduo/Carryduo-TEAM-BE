f8102c6869a38245da467862fb0f02504683f97d
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-03 20:33:31 +0900

    docs: 사용되지 않는 내용들 제거

--- src/combination-stat/entities/combination-stat.entity.ts
+++ src/combination-stat/entities/combination-stat.entity.ts
@@ -1,71 +1,29 @@
 import { CommonEntity } from '../../common/entities/common.entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ChampEntity } from 'src/champ/entities/champ.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'COMBINATION_STAT',
 })
 export class CombinationStatEntity extends CommonEntity {
-  @ApiProperty({
-    example: 'top-jungle/mid-jungle/ad-support/ 챔피언 상세 조회시: 챔피언id',
-    description: '조합승률을 보여줄',
-    required: false,
-  })
   @Column({ type: 'int', nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   category: number;
 
-  @ApiProperty({
-    example: '1',
-    description: '조합승률 데이터의 순위',
-    required: false,
-  })
   @Column({ type: 'int', nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   rankInCategory: number;
 
-  @ApiProperty({
-    example: '1',
-    description: '조합승률 데이터의 티어',
-    required: false,
-  })
   @Column({ type: 'int', nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   tier: number;
 
-  @ApiProperty({
-    example: '59.1',
-    description: '조합승률',
-    required: false,
-  })
   @Column({ type: 'decimal', precision: 5, scale: 2, nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   winrate: number;
 
-  @ApiProperty({
-    example: '595',
-    description: '표본수',
-    required: false,
-  })
   @Column({ type: 'int', nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   sampleNum: number;
 
   @ManyToOne(() => ChampEntity, (champEntity: ChampEntity) => champEntity.id, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
-  })
-  @ApiProperty({
-    example: '56',
-    description: '기준 챔피언 ID',
-    required: false,
   })
   @JoinColumn([
     {
@@ -78,11 +36,6 @@ export class CombinationStatEntity extends CommonEntity {
   @ManyToOne(() => ChampEntity, (champEntity: ChampEntity) => champEntity.id, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
-  })
-  @ApiProperty({
-    example: '56',
-    description: '조합 챔피언 ID',
-    required: false,
   })
   @JoinColumn([
     {
--- src/comments/pipes/comment.param.validation.pipe.ts
+++ src/comments/pipes/comment.param.validation.pipe.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   PipeTransform,
   ParseIntPipe,
   ParseUUIDPipe,
