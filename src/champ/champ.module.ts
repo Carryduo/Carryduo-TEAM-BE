4d5d5a4222463296db62cfff321385528aaa5bfa
rlasuddn <ywaves073@gmail.com>
2022-10-06 15:19:49 +0900

    feat: 챔프 스펠 데이터 응답 추가

--- src/champ/champ.module.ts
+++ src/champ/champ.module.ts
@@ -6,11 +6,17 @@ import { ChampController } from './champ.controller';
 import { ChampRepository } from './champ.repository';
 import { ChampService } from './champ.service';
 import { ChampEntity } from './entities/champ.entity';
+import { ChampSpellEntity } from './entities/champ.spell';
 import { ChampSkillInfoEntity } from './entities/champSkillInfo.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ChampEntity, ChampSkillInfoEntity, UserEntity]),
+    TypeOrmModule.forFeature([
+      ChampEntity,
+      ChampSkillInfoEntity,
+      ChampSpellEntity,
+      UserEntity,
+    ]),
     HttpModule,
   ],
   controllers: [ChampController],
