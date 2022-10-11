a5eafbec90f4971e15d38f99b1537b40cef2309b
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-11 20:04:46 +0900

    [#72] build: 대전 시뮬레이션 API 초기 세팅

---
+++ src/simulation/simulation.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SimulationController } from './simulation.controller';
+import { SimulationService } from './simulation.service';
+
+@Module({
+  controllers: [SimulationController],
+  providers: [SimulationService]
+})
+export class SimulationModule {}
