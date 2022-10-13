3ed201a76200c3a88e3b9f57ead26123640cb9e2
rlasuddn <ywaves073@gmail.com>
2022-10-13 22:46:35 +0900

    docs: Throttler 해제

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,6 @@ import { CombinationStatEntity } from './combination-stat/entities/combination-s
 import { SubscriptionEntity } from './subscription/entities/subscription.entity';
 import { SummonerHistoryEntity } from './summoner/entities/summoner.history.entity';
 import { ChampSpellEntity } from './champ/entities/champ.spell';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { SimulationModule } from './simulation/simulation.module';
 import { SimulationEntity } from './simulation/entities/simulation.entity';
 
@@ -61,10 +60,6 @@ const typeOrmModuleOptions = {
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 1,
-    }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
     AdminModule,
