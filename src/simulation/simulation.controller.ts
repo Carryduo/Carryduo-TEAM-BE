1f8cd2019f4d2bf1a4b120ec4eedf17999cc1837
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-14 17:49:05 +0900

    [#72] response 데이터 수정 및 query string 예외처리

--- src/simulation/simulation.controller.ts
+++ src/simulation/simulation.controller.ts
@@ -1,12 +1,12 @@
 import { TierListParamPipe } from './../combination-stat/pipes/combination-stat.param.validator.pipe';
 import { SimulationService } from './simulation.service';
 import {
-  Body,
   Controller,
   Get,
   Param,
   Query,
   UseFilters,
+  HttpException,
 } from '@nestjs/common';
 import {
   ApiOperation,
@@ -16,7 +16,6 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/exception/http-exception.filter';
-import { simulationBodyDto } from './dto/simulation.request.dto';
 import { simulationResponseDto } from './dto/simulation.response.dto';
 
 @Controller('simulation')
@@ -69,6 +68,14 @@ export class SimulationController {
     @Query('champ3Id') champ3Id: string,
     @Query('champ4Id') champ4Id: string,
   ): Promise<simulationResponseDto> {
+    if (
+      isNaN(Number(champ1Id)) ||
+      isNaN(Number(champ2Id)) ||
+      isNaN(Number(champ3Id)) ||
+      isNaN(Number(champ4Id))
+    ) {
+      throw new HttpException(`champId는 숫자형태여야 합니다.`, 400);
+    }
     return this.simulaitionService.getSimulationData(
       category,
       champ1Id,
--- src/simulation/simulation.service.ts
+++ src/simulation/simulation.service.ts
@@ -7,11 +7,12 @@ export class SimulationService {
   constructor(private readonly simulationRepository: SimulationRepository) {}
   async getSimulationData(
     category: string | number,
-    champ1Id,
-    champ2Id,
-    champ3Id,
-    champ4Id,
+    champ1Id: string | number,
+    champ2Id: string | number,
+    champ3Id: string | number,
+    champ4Id: string | number,
   ) {
+    console.log(champ1Id, champ2Id, champ3Id, champ4Id);
     switch (category) {
       case 'top-jungle':
         category = 0;
@@ -24,25 +25,46 @@ export class SimulationService {
         break;
     }
 
-    const option1 = new Brackets((qb) => {
-      qb.where('SIMULATION.champ1Id = :champ1Id', {
-        champ1Id,
-      }).andWhere('SIMULATION.champ2Id = :champ2Id', {
-        champ2Id,
-      });
+    const option = new Brackets((qb) => {
+      qb.where('SIMULATION.category = :category', {
+        category,
+      })
+        .andWhere('SIMULATION.champ1Id = :champ1Id', {
+          champ1Id,
+        })
+        .andWhere('SIMULATION.champ2Id = :champ2Id', {
+          champ2Id,
+        })
+        .andWhere('SIMULATION.champ3Id = :champ3Id', {
+          champ3Id,
+        })
+        .andWhere('SIMULATION.champ4Id = :champ4Id', {
+          champ4Id,
+        });
     });
-    const option2 = new Brackets((qb) => {
-      qb.where('SIMULATION.champ3Id = :champ3Id', {
-        champ3Id,
-      }).andWhere('SIMULATION.champ4Id = :champ4Id', {
-        champ4Id,
-      });
+
+    const reverseOption = new Brackets((qb) => {
+      qb.where('SIMULATION.category = :category', {
+        category,
+      })
+        .andWhere('SIMULATION.champ1Id = :champ3Id', {
+          champ3Id,
+        })
+        .andWhere('SIMULATION.champ2Id = :champ4Id', {
+          champ4Id,
+        })
+        .andWhere('SIMULATION.champ3Id = :champ1Id', {
+          champ1Id,
+        })
+        .andWhere('SIMULATION.champ4Id = :champ2Id', {
+          champ2Id,
+        });
     });
-    const data = await this.simulationRepository.getSimulationData(
-      category,
-      option1,
-      option2,
-    );
+
+    let data = await this.simulationRepository.getSimulationData(option);
+    if (!data) {
+      data = await this.simulationRepository.getSimulationData(reverseOption);
+    }
     const champ1 = data.champ1Id;
     const champ2 = data.champ2Id;
     if (champ1Id === data.champ3Id.id) {
@@ -53,6 +75,13 @@ export class SimulationService {
       data.champ4Id = champ2;
     } else {
       data.winrate = Number((data.winrate * 100).toFixed(2));
+    }
+    if (data.winrate > 50) {
+      data.winrate = 0;
+    } else if (data.winrate === 50) {
+      data.winrate = 1;
+    } else {
+      data.winrate = 2;
     }
     return data;
   }
