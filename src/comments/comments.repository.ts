44bcdcc5b579bc9517d2b0f1db2373c7f9ffb64f
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-17 18:31:43 +0900

    [#12] refactor: 쿼리 조건문 분기점 리팩토링

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AdminResponseDTO } from 'src/admin/dto/admin.response';
 import { Repository } from 'typeorm';
-import { PostCommentDTO } from './dto/comment.request.dto';
 import { CommentEntity } from './entities/comments.entity';
 
 @Injectable()
@@ -14,98 +12,66 @@ export class CommentRepository {
   //   TODO: 코드 사용성 개선 (쿼리가 불필요하게 많음)
 
   // : Promise<UserBasicInfoResponseDTO[]>
-  async getComments(category: string, target: string) {
-    let data;
-    if (category === 'champ') {
-      data = await this.commentsRepository
-        .createQueryBuilder('comment')
-        .leftJoinAndSelect('comment.userId', 'user')
-        .leftJoinAndSelect('comment.champId', 'champ')
-        .leftJoinAndSelect('comment.summonerName', 'summoner')
-        .select([
-          'comment.content',
-          'comment.id',
-          'user.id',
-          'user.profileImg',
-          'user.nickname',
-          'champ.id',
-          'comment.reportNum',
-          'comment.createdAt',
-          'comment.category',
-          'comment.summonerName',
-          'summoner.summonerName',
-        ])
-        .where('comment.category = :category', { category })
-        .andWhere('comment.champId = :champId', { champId: target })
-        .orderBy({
-          'comment.createdAt': 'DESC',
-        })
-        .getMany();
-    } else if (category === 'summoner') {
-      data = await this.commentsRepository
-        .createQueryBuilder('comment')
-        .leftJoinAndSelect('comment.userId', 'user')
-        .leftJoinAndSelect('comment.champId', 'champ')
-        .leftJoinAndSelect('comment.summonerName', 'summoner')
-        .select([
-          'comment.content',
-          'comment.id',
-          'user.id',
-          'user.profileImg',
-          'user.nickname',
-          'champ.id',
-          'comment.reportNum',
-          'comment.createdAt',
-          'comment.category',
-          'comment.summonerName',
-          'summoner.summonerName',
-        ])
-        .where('comment.category = :category', { category })
-        .andWhere('comment.summonerName = :summonerName', {
-          summonerName: target,
-        })
-        .orderBy({
-          'comment.createdAt': 'DESC',
-        })
-        .getMany();
-    }
-    return data;
+  async getComments(option) {
+    return await this.commentsRepository
+      .createQueryBuilder('comment')
+      .leftJoinAndSelect('comment.userId', 'user')
+      .leftJoinAndSelect('comment.champId', 'champ')
+      .leftJoinAndSelect('comment.summonerName', 'summoner')
+      .select([
+        'comment.content',
+        'comment.id',
+        'user.id',
+        'user.profileImg',
+        'user.nickname',
+        'champ.id',
+        'comment.reportNum',
+        'comment.createdAt',
+        'comment.category',
+        'comment.summonerName',
+        'summoner.summonerName',
+      ])
+      .where(option)
+      .orderBy({
+        'comment.createdAt': 'DESC',
+      })
+      .getMany();
   }
-  async postComment(
-    category: string,
-    target: string,
-    user: AdminResponseDTO,
-    data: PostCommentDTO,
-  ) {
+  // TODO: QUERY 분기점 service로 옮기기
+  async postComment(value, option) {
+    const { target, category } = option;
     // 챔피언 댓글
-    if (category === 'champ') {
-      await this.commentsRepository
-        .createQueryBuilder()
-        .insert()
-        .into(CommentEntity)
-        .values({
-          userId: user.userId,
-          category,
-          champId: target,
-          content: data.content,
-        })
-        .execute();
-    }
-    // 소환사 댓글
-    else if (category === 'summoner') {
-      await this.commentsRepository
-        .createQueryBuilder()
-        .insert()
-        .into(CommentEntity)
-        .values({
-          // Promise로 묶인 column은 다음과 같이 function 형태로 주입해주어야 한다.
-          userId: user.userId,
-          category,
-          summonerName: target,
-          content: data.content,
-        })
-        .execute();
-    }
+    await this.commentsRepository
+      .createQueryBuilder()
+      .insert()
+      .into(CommentEntity)
+      .values(value)
+      .execute();
+
+    const result = await this.commentsRepository
+      .createQueryBuilder('comment')
+      .leftJoinAndSelect('comment.userId', 'user')
+      .leftJoinAndSelect('comment.champId', 'champ')
+      .leftJoinAndSelect('comment.summonerName', 'summoner')
+      .select([
+        'comment.content',
+        'comment.id',
+        'user.id',
+        'user.profileImg',
+        'user.nickname',
+        'champ.id',
+        'comment.reportNum',
+        'comment.createdAt',
+        'comment.category',
+        'comment.summonerName',
+        'summoner.summonerName',
+      ])
+      .where(option)
+      .orderBy({
+        'comment.createdAt': 'DESC',
+      })
+      .getMany();
+
     return { success: true, message: '평판 업로드 완료했습니다' };
   }
 
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -3,16 +3,33 @@ import { PostCommentDTO } from './dto/comment.request.dto';
 import { Injectable, HttpException } from '@nestjs/common';
 import { AdminResponseDTO } from 'src/admin/dto/admin.response';
 import { CommentGetResponseDTO } from './dto/comment.response.dto';
+import { Brackets } from 'typeorm';
 
 @Injectable()
 export class CommentsService {
   constructor(private readonly commentRepository: CommentRepository) {}
 
-  async getComments(
-    category: string,
-    target: string,
-  ): Promise<CommentGetResponseDTO> {
-    return await this.commentRepository.getComments(category, target);
+  // Promise<CommentGetResponseDTO>
+  async getComments(category: string, target: string) {
+    let option;
+    if (category === 'champ') {
+      option = new Brackets((qb) => {
+        qb.where('comment.category = :category', { category }).andWhere(
+          'comment.champId = :champId',
+          { champId: target },
+        );
+      });
+    } else {
+      option = new Brackets((qb) => {
+        qb.where('comment.category = :category', { category }).andWhere(
+          'comment.summonerName = :summonerName',
+          {
+            summonerName: target,
+          },
+        );
+      });
+    }
+    return await this.commentRepository.getComments(option);
   }
 
   postComment(
@@ -21,7 +38,37 @@ export class CommentsService {
     user: AdminResponseDTO,
     data: PostCommentDTO,
   ) {
-    return this.commentRepository.postComment(category, target, user, data);
+    let value, option;
+    if ((category = 'champ')) {
+      value = {
+        userId: user.userId,
+        category,
+        champId: target,
+        content: data.content,
+      };
+      option = new Brackets((qb) => {
+        qb.where('comment.category = :category', { category }).andWhere(
+          'comment.champId = :champId',
+          { champId: target },
+        );
+      });
+    } else {
+      value = {
+        userId: user.userId,
+        category,
+        summonerName: target,
+        content: data.content,
+      };
+      option = new Brackets((qb) => {
+        qb.where('comment.category = :category', { category }).andWhere(
+          'comment.summonerName = :summonerName',
+          {
+            summonerName: target,
+          },
+        );
+      });
+    }
+    return this.commentRepository.postComment(value, option);
   }
 
   async updateReportNum(id: string) {
