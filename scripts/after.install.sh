6a29ae4d01bc0bfd797b6eddb0ff598bc166727d
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-19 16:58:14 +0900

    build: 파이프라인 코드 수정(src 유지)

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -3,6 +3,6 @@ sudo chmod -R 777 /home/ubuntu/build
 
 #navigate into our working directory
 cd /home/ubuntu/build
-
+sudo npm install
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload carryduo
