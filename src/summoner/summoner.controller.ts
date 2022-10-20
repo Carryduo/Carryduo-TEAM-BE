245a06e7965b09ff0dbfd134a82a56eaceaaff79
rlasuddn <ywaves073@gmail.com>
2022-10-20 22:01:51 +0900

    feat: typeorm exception filter 생성 및 적용

--- src/summoner/summoner.controller.ts
+++ src/summoner/summoner.controller.ts
@@ -9,12 +9,13 @@ import {
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HttpCacheInterceptor } from 'src/common/cache-interceptor/cache.interceptor';
 import { HttpExceptionFilter } from 'src/common/exception/http-exception.filter';
+import { TypeOrmFilter } from 'src/common/exception/typeorm-exception.filter';
 import { SummonerAllDataDTO } from './dto/summoner/summoner.data.dto';
 import { SummonerService } from './summoner.service';
 
 @Controller('summoner')
 @ApiTags('Summoner')
-@UseFilters(HttpExceptionFilter)
+@UseFilters(HttpExceptionFilter, TypeOrmFilter)
 export class SummonerController {
   constructor(private readonly summonerService: SummonerService) {}
 
