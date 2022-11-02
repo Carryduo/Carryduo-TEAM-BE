28f63fbb97b367fe77d5063e6f1ecd21f547af7e
rlasuddn <ywaves073@gmail.com>
2022-11-02 16:56:59 +0900

    fix: champDetailDTO 수정
     - IntersectionType으로 champ, rate 통합

--- src/champ/dto/champ-detail/champ.detail.common.dto.ts
+++ src/champ/dto/champ-detail/champ.detail.common.dto.ts
@@ -6,6 +6,7 @@ export class ChampDetailCommonDTO {
     example: 'q',
     description: 'q | w | e | r | passive',
     required: true,
+    name: 'id',
   })
   @IsString()
   @IsNotEmpty()
@@ -15,6 +16,7 @@ export class ChampDetailCommonDTO {
     example: '결정타',
     description: 'q | w | e | r | passive의 스킬 이름',
     required: true,
+    name: 'name',
   })
   @IsString()
   @IsNotEmpty()
@@ -24,6 +26,7 @@ export class ChampDetailCommonDTO {
     example: '가렌의 이동 속도가 큰 폭으로 증가하고.....',
     description: 'q | w | e | r | passive의 스킬 설명',
     required: true,
+    name: 'desc',
   })
   @IsString()
   @IsNotEmpty()
@@ -33,6 +36,7 @@ export class ChampDetailCommonDTO {
     example: '가렌에게 적용된 모든 둔화 효과가 제거되고.....',
     description: 'q | w | e | r | passive의 스킬 툴팁',
     required: true,
+    name: 'toolTip',
   })
   @IsString()
   @IsNotEmpty()
@@ -42,6 +46,7 @@ export class ChampDetailCommonDTO {
     example: 'example.png',
     description: 'q | w | e | r | passive의 스킬 이미지',
     required: true,
+    name: 'image',
   })
   @IsString()
   @IsNotEmpty()
