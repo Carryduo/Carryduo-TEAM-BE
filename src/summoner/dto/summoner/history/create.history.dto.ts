89e51202b24a31455ef6338a76e98c82eb916243
rlasuddn <ywaves073@gmail.com>
2023-02-19 23:44:28 +0900

    fix: CreateSummonerHistoryDto pickTyep 적용

---
+++ src/summoner/dto/summoner/history/create.history.dto.ts
@@ -0,0 +1,29 @@
+import { PickType } from '@nestjs/swagger';
+import { SummonerHistoryEntity } from 'src/summoner/entities/summoner.history.entity';
+import { SummonerHistoryCommonDto } from './history.common.dto';
+
+export class CreateSummonerHistoryDto extends PickType(SummonerHistoryCommonDto, [
+  'win',
+  'kill',
+  'death',
+  'assist',
+  'champId',
+  'position',
+  'summonerName',
+  'summonerId',
+  'matchId',
+]) {
+  toEntity() {
+    const summonerHistory = new SummonerHistoryEntity();
+    summonerHistory.win = this.win;
+    summonerHistory.kill = this.kill;
+    summonerHistory.death = this.death;
+    summonerHistory.assist = this.assist;
+    summonerHistory.champId = this.champId;
+    summonerHistory.position = this.position;
+    summonerHistory.summonerName = this.summonerName;
+    summonerHistory.summonerId = this.summonerId;
+    summonerHistory.matchId = this.matchId;
+    return summonerHistory;
+  }
+}
