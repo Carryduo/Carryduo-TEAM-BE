2b9325b598054e6b7c8d1c307213755e7cd3baef
rlasuddn <ywaves073@gmail.com>
2023-02-22 20:08:44 +0900

    feat: 소환사 전적 갱신 같은 소환사 api요청 5분 제한 인터셉터 추가

---
+++ src/common/interceptors/throttler.interceptors.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ThrottleInterceptor implements NestInterceptor {
+  private readonly cache = new Map<string, number>();
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const summonerName = request.params.summonerName;
+    const requestTime = Date.now();
+
+    if (this.cache.has(summonerName)) {
+      const lastRequestTime = this.cache.get(summonerName);
+
+      if (requestTime - lastRequestTime < 5 * 60 * 1000) {
+        throw new HttpException('Too many requests', HttpStatus.TOO_MANY_REQUESTS);
+      }
+    }
+
+    this.cache.set(summonerName, requestTime);
+
+    return next.handle();
+  }
+}
