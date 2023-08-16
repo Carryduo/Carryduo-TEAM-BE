f97b9790049c8399465e728e697646e61c332ce8
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-16 19:35:30 +0900

    feat: add script to create log stream in cloudwatch

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -1,7 +1,12 @@
+
+echo ${COMMIT_ID}
 #!/bin/bash
 sudo chmod -R 777 /home/ubuntu/build
-
+sudo chmod 400 /home/ubuntu/build/scripts/create.log-stream.sh
 #navigate into our working directory
 cd /home/ubuntu/build
+
+./scripts/create.log-stream.sh
+
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload carryduo
