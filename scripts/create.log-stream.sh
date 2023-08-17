7645b3b654f3c84e746c04b50e0e821fd7ad645d
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-17 20:07:10 +0900

    fix: fix build, appspec.yml and create.log-stream.sh

--- scripts/create.log-stream.sh
+++ scripts/create.log-stream.sh
@@ -1,3 +1,3 @@
-COMMIT_ID=$(git rev-parse HEAD)
-export COMMIT_ID
-sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $COMMIT_ID
+source ./build.env
+
+sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $CODEBUILD_RESOLVED_SOURCE_VERSION
