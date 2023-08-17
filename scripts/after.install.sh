c888efa4a9fb2e3a3d24c39df9e8d58a739d03ec
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-17 19:53:20 +0900

    fix: fix appspec.yml

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -6,7 +6,5 @@ sudo chmod 400 /home/ubuntu/build/scripts/create.log-stream.sh
 #navigate into our working directory
 cd /home/ubuntu/build
 
-sudo ./scripts/create.log-stream.sh
-
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload carryduo
