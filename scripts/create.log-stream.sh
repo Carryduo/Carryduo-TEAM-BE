0dc56fda83f56365668c63816996276afadbbdc8
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-17 20:43:35 +0900

    fix file name

--- scripts/create.log-stream.sh
+++ scripts/create.log-stream.sh
@@ -1,5 +1,5 @@
 sudo chmod -R 777 /home/ubuntu/build
 cd /home/ubuntu/build
-source build.env
-echo $CODEBUILD_RESOLVED_SOURCE_VERSION
+source build.sh
+echo 'commit id: $CODEBUILD_RESOLVED_SOURCE_VERSION'
 sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $CODEBUILD_RESOLVED_SOURCE_VERSION
