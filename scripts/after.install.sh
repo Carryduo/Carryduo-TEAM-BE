9b16ea4ac1fe9073766a5a5f7857a06ed55076aa
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-17 21:24:34 +0900

    fix: add region in command

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -5,7 +5,7 @@ cd /home/ubuntu/build
 sudo chmod +x /home/ubuntu/build/build.sh 
 source build.sh
 echo 'commit id' $CODEBUILD_RESOLVED_SOURCE_VERSION
-sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $CODEBUILD_RESOLVED_SOURCE_VERSION
+sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $CODEBUILD_RESOLVED_SOURCE_VERSION --region ap-northeast-2
 
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload carryduo
