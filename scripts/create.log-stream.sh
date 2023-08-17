ac16e5d127ef3b784548b30a1ac39d8a1fe314eb
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-17 19:39:16 +0900

    fix: fix script

--- scripts/create.log-stream.sh
+++ scripts/create.log-stream.sh
@@ -1,3 +1,3 @@
 COMMIT_ID=$(git rev-parse HEAD)
 export COMMIT_ID
-aws logs create-log-stream --log-group-name carryduo --log-stream-name $COMMIT_ID
+sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $COMMIT_ID
