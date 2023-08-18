522d019c1663bb7424b6e58e70272ea15ad48ad0
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-18 17:25:31 +0900

    reset config to yesterday

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -4,8 +4,8 @@ sudo chmod -R 777 /home/ubuntu/build
 cd /home/ubuntu/build
 sudo chmod +x /home/ubuntu/build/build.sh 
 source build.sh
-COMMIT_ID=${SourceVariables.CommitId}
-sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $COMMIT_ID --region ap-northeast-2
+echo 'commit id' $CODEBUILD_RESOLVED_SOURCE_VERSION
+sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $CODEBUILD_RESOLVED_SOURCE_VERSION --region ap-northeast-2
 
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload ecosystem.json
