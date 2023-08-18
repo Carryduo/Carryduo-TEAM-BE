a99ce12364ff23ce532532e1ab3f78a540722344
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-08-18 16:24:15 +0900

    fix commit id variable

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -4,8 +4,9 @@ sudo chmod -R 777 /home/ubuntu/build
 cd /home/ubuntu/build
 sudo chmod +x /home/ubuntu/build/build.sh 
 source build.sh
-echo 'commit id' $CODEBUILD_RESOLVED_SOURCE_VERSION
-sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name ${SourceVariables.CommitId} --region ap-northeast-2
+echo 'commit id: #{SourceVariables.CommitId} '
+COMMIT_ID=#{SourceVariables.CommitId}
+sudo aws logs create-log-stream --log-group-name carryduo --log-stream-name $COMMIT_ID --region ap-northeast-2
 
 #install node modules & update swagger & pm2 reload
 sudo pm2 reload ecosystem.json
